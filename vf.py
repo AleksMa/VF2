78657437e6c5afe93282d15601996371b07f283d
Alexey Mamaev <a.mamaev@corp.mail.ru>
2021-05-24 22:20:27 +0300

    VF2: [*] refactoring

--- vf.py
+++ vf.py
@@ -26,7 +26,7 @@ def CreateGraph(filename):
                     G.add_edge(int(lineList[1]),int(lineList[2]),weight=int(lineList[3]))
                 else:
                     #empty line!
-                    continue           
+                    continue
     except(IOError):
         print("Class GraphSet __init__() Cannot open Graph file", filename)
         exit()
@@ -36,7 +36,7 @@ class Vf:
 
     __origin = None
     __sub = None
-     
+
     def candidate(self, subMNeighbor, gMNeighbor):
         if not (subMNeighbor and gMNeighbor):
             print("Class Vf candidate() arguments value error! subMNeighbor or gMNeighbor is empty!")
@@ -47,8 +47,8 @@ class Vf:
         if not all(isinstance(x, int) for x in subMNeighbor):
             print("Class Vf candidate() arguments type error! int in subMNeighbor list expected!")
         if not all(isinstance(x, int) for x in gMNeighbor):
-            print("Class Vf candidate() arguments type error! int in gMNeighbor list expected!")        
-        
+            print("Class Vf candidate() arguments type error! int in gMNeighbor list expected!")
+
         pairs = []
         for i in range(len(subMNeighbor)):
             for j in range(len(gMNeighbor)):
@@ -56,7 +56,7 @@ class Vf:
                 pairs.append(string)
         return pairs
 
-    #type = 0, pre; type = 1, succ    
+    #type = 0, pre; type = 1, succ
     def preSucc(self, vertexNeighbor, map, type):
         #vertexNeighbor and map can be empty
         if not (isinstance(vertexNeighbor, list) and isinstance(map, list)):
@@ -64,12 +64,12 @@ class Vf:
             exit()
         if not (type == 0 or type == 1):
             print("Class Vf preSucc() arguments value error! type expected 0 or 1!")
-           
+
         result = []
         #succ
         if type:
             for vertex in vertexNeighbor:
-                if vertex not in map:                   
+                if vertex not in map:
                     result.append(vertex)
         #pre
         else:
@@ -77,16 +77,16 @@ class Vf:
                 if vertex in map:
                     result.append(vertex)
         return result
-    
+
     #type = 0, __sub; type = 1, __origin
     def edgeLabel(self, offset, index1, index2, type):
         '''
         if(int(index1) < int(index2)):
             key = str(index1) + ":" + str(index2)
         else:
-            key = str(index2) + ":" + str(index1) 
+            key = str(index2) + ":" + str(index1)
         '''
-        key = str(index1) + ":" + str(index2) 
+        key = str(index1) + ":" + str(index2)
         if type:
             ESet = self.__origin.curESet(offset)
         else:
@@ -95,8 +95,8 @@ class Vf:
             return ESet[key]
         else:
             return ESet[str(index2) + ":" + str(index1)]
-         
-    
+
+
     def isMatchInV2Succ(self, j, vertex, edge, v2, v2Succ):
         for succ in v2Succ:
             vLabel = self.__origin.curVSet(j)[succ]
@@ -104,10 +104,10 @@ class Vf:
             if vLabel == vertex and eLabel == edge:
                 return True
         return False
-        
+
 
     def isMeetRules(self, v1, v2, i, j, result, subMap, gMap, subMNeighbor, gMNeighbor):
-        
+
         '''
         #test usage!
         print "-------------------------------------------"
@@ -118,24 +118,24 @@ class Vf:
         print "in isMeetRules() subMNeighbor: ", subMNeighbor
         print "in isMeetRules() gMNeighbor: ", gMNeighbor
         '''
-        
+
         #compare label of v1 and v2
         subVSet = self.__sub.curVSet(i)
         gVSet = self.__origin.curVSet(j)
-        
+
 
         if subVSet[v1] != gVSet[v2]:
             #print "vertex label different!"
             return False
-            
+
         #notice, when result is empty, first pair should be added when their vertexLabels are the same!
         if not result:
             return True
-        
-        
+
+
         v1Neighbor = self.__sub.neighbor(i, v1)
         v2Neighbor = self.__origin.neighbor(j, v2)
-                
+
         v1Pre = self.preSucc(v1Neighbor, subMap, 0)
         v1Succ = self.preSucc(v1Neighbor, subMap, 1)
         v2Pre = self.preSucc(v2Neighbor, gMap, 0)
@@ -155,7 +155,7 @@ class Vf:
         if(len(v1Pre) > len(v2Pre)):
             #print "len(v1Pre) > len(v2Pre)!"
             return False
-                
+
         for pre in v1Pre:
             if result[pre] not in v2Pre:
                 #print "v1Pre not in v2Pre!"
@@ -163,7 +163,7 @@ class Vf:
             if self.edgeLabel(i, v1, pre, 0) != self.edgeLabel(j, v2, result[pre], 1):
                 #print "eLabel of v1-pre different with eLabel of v2-result[pre]!"
                 return False
-           
+
         '''   
         if(len(v1Succ) > len(v2Succ)):
             #print "len(v1Succ) > len(v2Succ)!"
@@ -176,31 +176,31 @@ class Vf:
                 #print "not self.isMatchInV2Succ()"
                 return False
         '''
-        
+
         #5,6 rules
         len1 = len(set(v1Neighbor) & set(subMNeighbor))
         len2 = len(set(v2Neighbor) & set(gMNeighbor))
         if len1 > len2:
             #print "5,6 rules mismatch!"
             return False
-            
-        #7 rule     
+
+        #7 rule
         len1= len(set(self.__sub.curVSet(i).keys()) - set(subMNeighbor) - set(v1Succ))
         len2 = len(set(self.__origin.curVSet(j).keys()) - set(gMNeighbor) - set(v2Succ))
         if len1 > len2:
             #print "7 rule mismatch!"
-            return False        
-            
-        return True
-        
+            return False
 
-    def dfsMatch(self, i, j, result):   
+        return True
+
+
+    def dfsMatch(self, i, j, result):
         #print "in dfsMatch() result: ", result
         if not isinstance(result, dict):
             print("Class Vf dfsMatch() arguments type error! result expected dict!")
-        
+
         curMap = Map(result)
-        
+
         '''
         #test usage!
         print "in dfsMatch() curMap.subMap() : ", curMap.subMap()
@@ -208,23 +208,23 @@ class Vf:
         print "in dfsMatch() self.__sub.curVSet(i) : ", self.__sub.curVSet(i)
         print "in dfsMatch() self.__sub.curVSet(i) length: ", len(self.__sub.curVSet(i))
         '''
-                
+
         if curMap.isCovered(self.__sub.curVSet(i)):
             return result
-        
- 
+
+
         subMNeighbor = curMap.neighbor(i, self.__sub, 0, True)
-        gMNeighbor = curMap.neighbor(j, self.__origin, 1, True)   
-       
+        gMNeighbor = curMap.neighbor(j, self.__origin, 1, True)
+
         if not (subMNeighbor and gMNeighbor):
             print("Class Vf dfsMatch(), subMNeighbor or gMNeighbor is empty!")
             exit()
-        
+
         subNMNeighbor = curMap.neighbor(i, self.__sub, 0, False)
         gNMNeighbor = curMap.neighbor(j, self.__origin, 1, False)
         #print "in dfsMatch() subNMNeighbor: ", subNMNeighbor
         #print "in dfsMatch() gNMNeighbor: ", gNMNeighbor
-        
+
         #notice, choose one vertex in subGraphNeighbor is ok
         while(len(subNMNeighbor) > 1):
             subNMNeighbor.pop()
@@ -239,23 +239,23 @@ class Vf:
         pairs = self.candidate(subMNeighbor, gMNeighbor)
         print "Class Vf dfsMatch() pairs: ", pairs
         '''
-        
-        pairs = self.candidate(subNMNeighbor, gNMNeighbor)        
+
+        pairs = self.candidate(subNMNeighbor, gNMNeighbor)
         if not pairs:
             return result
-                
-        for pair in pairs:        
+
+        for pair in pairs:
             v1, v2 = pair.strip().split(":")
             if(self.isMeetRules(int(v1), int(v2), i, j, result, curMap.subMap(), curMap.gMap(), subMNeighbor, gMNeighbor)):
-                result[int(v1)] = int(v2)       
-                self.dfsMatch(i, j, result)                 
+                result[int(v1)] = int(v2)
+                self.dfsMatch(i, j, result)
                 #notice, it's important to return result when len(result) == len(self.__sub.curVSet(i))
                 #otherwise it will continue to pop
                 if len(result) == len(self.__sub.curVSet(i)):
                     return result
-                result.pop(int(v1))                
+                result.pop(int(v1))
         return result
-        
+
 
     def run(self, f1, f2):
         g1 = CreateGraph(f1)
@@ -263,7 +263,7 @@ class Vf:
 
         return self.main(g1, g2)
 
-    def main(self, g1, g2):   
+    def main(self, g1, g2):
         # output = sys.stdout
         '''
         output = sys.stdout
@@ -273,34 +273,23 @@ class Vf:
 
         self.__origin = GraphSet(g1)
         self.__sub = GraphSet(g2)
-        
-        
+
+
         # #test usage!
         # print "in main() subVSet: ", self.__sub.curVSet(0)
-        # print "in main() graphVSet: ", self.__origin.curVSet(0)       
+        # print "in main() graphVSet: ", self.__origin.curVSet(0)
         # print "in main() subVESet: ", self.__sub.curVESet(0)
         # print "in main() gVESet: ", self.__origin.curVESet(0)
-        
-        
+
+
         subLen = len(self.__sub.graphSet())
         gLen = len(self.__origin.graphSet())
-        
-        for i in range(subLen):          
+
+        for i in range(subLen):
             for j in range(gLen):
                 result = {}
-                result = self.dfsMatch(i, j, result)                              
+                result = self.dfsMatch(i, j, result)
                 if len(result) == len(self.__sub.curVSet(i)):
-                    return result                    
+                    return result
                 else:
                     return {}
-                    
-                
-
-        
-    
-    
-   
-# in main() subVESet:  [['0:8', '0:5', '0:6', '0:1'], ['1:3', '1:4', '1:2', '0:1', '1:5', '1:7'], ['1:2'], ['1:3', '3:5'], ['1:4'], ['0:5', '1:5', '5:8', '5:6', '3:5'], ['0:6', '5:6'], ['7:8', '1:7'], ['0:8', '7:8', '5:8']]
-# in main() subVESet:  [['0:8', '0:5', '0:6', '0:1'], ['1:3', '1:4', '1:2', '0:1', '1:5', '1:7'], ['1:2'], ['1:3', '3:5'], ['1:4'], ['0:5', '1:5', '5:8', '5:6', '3:5'], ['0:6', '5:6'], ['7:8', '1:7'], ['0:8', '7:8', '5:8']]
-
-
